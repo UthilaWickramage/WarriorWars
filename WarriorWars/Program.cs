818743530eae2b0fb36c0678a08310d2fef9b653
uthil <uthil@AXCITO-DEV-01>
2024-03-19 14:56:49 +0530

    Add project files.

---
+++ WarriorWars/Program.cs
@@ -0,0 +1,25 @@
+﻿using WarriorWars;
+using WarriorWars.Enum;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        Warrior goodGuy = new Warrior("John",Faction.GoodGuy);
+        Warrior badGuy = new Warrior("Jack",Faction.BadGuy);
+
+        while (goodGuy.IsAlive && badGuy.IsAlive)
+        {
+            if (random.Next(0, 10) < 5)
+            {
+                goodGuy.attack(badGuy);
+            }
+            else
+            {
+                badGuy.attack(goodGuy);
+            }
+            Thread.Sleep(50);
+        }
+    }
+}
---
+++ WarriorWars/Warrior.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarriorWars.Enum;
+using WarriorWars.Equipment;
+
+namespace WarriorWars
+{
+    class Warrior
+    {
+        private const int GOOD_GUY_STARTING_HEALTH = 100;
+        private const int BAD_GUY_STARTING_HEALTH = 100;
+
+
+        private readonly Faction FACTION;
+        private int health;
+        private string name;
+        private bool isAlive;
+
+        public bool IsAlive {
+            get
+            {
+                return isAlive;
+            }
+        }
+        private Weapon weapon;
+        private Armor armor;
+
+        public Warrior(String name,Faction faction)
+        {
+            this.name = name;
+            FACTION = faction;
+            this.isAlive = true;
+
+            switch (faction) {
+                case Faction.GoodGuy:
+                    armor = new Armor(faction);
+                    weapon = new Weapon(faction);
+                    health = GOOD_GUY_STARTING_HEALTH;
+                    break;
+                case Faction.BadGuy:
+                    weapon = new Weapon(faction);
+                    armor = new Armor(faction);
+                    health = BAD_GUY_STARTING_HEALTH;
+                    break;
+                    default: 
+                    break;
+}
+        }
+
+        public void attack(Warrior enemy)
+        {
+            int damage = weapon.Damage / enemy.armor.ArmorPoints;
+
+            enemy.health -= damage;
+
+            AttackResult(enemy, damage);
+        }
+
+        private void AttackResult(Warrior enemy, int damage)
+        {
+            if (enemy.health <= 0)
+            {
+                enemy.isAlive = false;
+                Tools.colorfulLine($"{enemy.name} is dead!", ConsoleColor.Red);
+                Tools.colorfulLine($"{name} is victorious", ConsoleColor.Green);
+            }
+            else
+            {
+                Console.WriteLine($"{name} just attacked {enemy.name} inflicted a damage of {damage}, The health is {enemy.health}");
+            }
+        }
+    }
+}
